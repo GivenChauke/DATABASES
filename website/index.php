169b77c5889c5219068023dacc411bed94603082
GivenChauke <chaukemnashe123@gmail.com>
2023-06-05 02:04:08 +0200

    Add rating system to the homePage of the site

--- website/index.php
+++ website/index.php
@@ -6,6 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>HomePage</title>
         <link rel="stylesheet" href="css/indexStyle.css"/>
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet"/>
     </head>
     <body>
         <?php include ("header.php"); ?>
@@ -50,8 +51,60 @@ req.onreadystatechange = function() {
 
             div.setAttribute('id','span');
             div.appendChild(img);
-            div.innerHTML+= "<p>Wine name: "+arr[i].WineName+"<br>Price: "+arr[i].Price+"<br>Wine type: "+ arr[i].Type+"<br>Region: "+arr[i].Region+"<p>";
-            imageContainer.appendChild(div);
+            div.innerHTML+= "<p>Wine name: "+arr[i].WineName+"<br>Price: "+arr[i].Price+"<br>Wine type: "+ 
+            arr[i].Type+"<br>Region: "+arr[i].Region+"<br>Rating: "+arr[i].AverageRating+"<p>";
+            
+            //code for rating system
+            const ratingClass = document.createElement('div');
+            ratingClass.classList.add('rating');
+
+            const star = document.createElement('input');
+            star.setAttribute('type', 'radio');
+            star.setAttribute('id', 'star' + i);
+            const label = document.createElement('label');
+            label.setAttribute('for', 'star' + i);
+            label.classList.add("fas", "fa-star");
+            ratingClass.appendChild(star);
+            ratingClass.appendChild(label);
+
+            const star1 = document.createElement('input');
+            star1.setAttribute('type', 'radio');
+            star1.setAttribute('id', 'star1' + i);
+            const label1 = document.createElement('label');
+            label1.setAttribute('for', 'star1' + i);
+            label1.classList.add("fas", "fa-star");
+            ratingClass.appendChild(star1);
+            ratingClass.appendChild(label1);
+
+            const star2 = document.createElement('input');
+            star2.setAttribute('type', 'radio');
+            star2.setAttribute('id', 'star2' + i);
+            const label2 = document.createElement('label');
+            label2.setAttribute('for', 'star2' + i);
+            label2.classList.add("fas", "fa-star");
+            ratingClass.appendChild(star2);
+            ratingClass.appendChild(label2);
+
+            const star3 = document.createElement('input');
+            star3.setAttribute('type', 'radio');
+            star3.setAttribute('id', 'star3' + i);
+            const label3 = document.createElement('label');
+            label3.setAttribute('for', 'star3' + i);
+            label3.classList.add("fas", "fa-star");
+            ratingClass.appendChild(star3);
+            ratingClass.appendChild(label3);
+
+            const star4 = document.createElement('input');
+            star4.setAttribute('type', 'radio');
+            star4.setAttribute('id', 'star4' + i);
+            const label4 = document.createElement('label');
+            label4.setAttribute('for', 'star4' + i);
+            label4.classList.add("fas", "fa-star");
+            ratingClass.appendChild(star4);
+            ratingClass.appendChild(label4);
+
+           div.appendChild(ratingClass);
+           imageContainer.appendChild(div);
         }
     }
 };
